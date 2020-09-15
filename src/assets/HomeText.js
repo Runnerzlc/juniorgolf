758635ba81675e33b1fb2e3e90340bc5d8ce3b44
RuiGan <rgan1@uci.edu>
2020-09-14 18:49:00 -0700

    add photo to about page and change font size

--- src/assets/HomeText.js
+++ src/assets/HomeText.js
@@ -1,8 +1,16 @@
 const textArray = [
-  "Overview of Junior Golf for Fun",
   "Junior Golf for Fun is a student-run organization that hosts free golf clinics taught by high school golfers to introduce kids of all backgrounds to the game of golf in fun and engaging ways. Along the way, kids learn to prevent injuries in their golf swings (which can be applied to all sports) and lessons that are applicable in their lives such as patience, independence, and integrity."
 ];
 
+const imageTextArray = [
+  "Fun",
+  "NCJGA",
+  "Free Golf Clinics",
+  "Beneficial",
+  "Injury Prevention"
+];
+
 export {
-  textArray
+  textArray,
+  imageTextArray
 };
